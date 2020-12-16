51ddf4515576e11293acf6ced6579bfebbb0148e
Jason Robinson <mail@jasonrobinson.me>
2020-12-16 23:02:44 +0200

    Add get_in_reply_to event util

--- middleman/utils.py
+++ middleman/utils.py
@@ -1,7 +1,22 @@
 import time
 
+from typing import Optional
+
+# noinspection PyPackageRequirements
+import nio
+
+
+def get_in_reply_to(event: nio.Event) -> Optional[str]:
+    """
+    Pulls an in reply to event ID from an event, if any.
+    """
+    return event.source.get("m.relates_to", {}).get("m.in_reply_to", {}).get("event_id")
+
 
 async def with_ratelimit(client, method, *args, **kwargs):
+    """
+    Call a client method with 3s backoff if rate limited.
+    """
     func = getattr(client, method)
     response = await func(*args, **kwargs)
     if getattr(response, "status_code", None) == "M_LIMIT_EXCEEDED":
