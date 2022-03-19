e4ee177a4ab9522c34a624cdb394a98a5ac5e1b6
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-03-19 11:31:57 +0200

    Add files via upload

--- middleman/migrations/001.py
+++ middleman/migrations/001.py
@@ -8,9 +8,25 @@ def migrate(store):
                 sender text
             )
         """)
+        store._execute("""
+            CREATE TABLE massages (
+                id SERIAL PRIMARY KEY,
+                event_id text constraint message_event_id_unique_idx unique,
+                room_id text,
+                sender text
+            )
+        """)
     else:
         store._execute("""
             CREATE TABLE messages (
+                id INTEGER PRIMARY KEY autoincrement,
+                event_id text constraint message_event_id_unique_idx unique,
+                room_id text,
+                sender text
+            )
+        """)
+        store._execute("""
+            CREATE TABLE massages (
                 id INTEGER PRIMARY KEY autoincrement,
                 event_id text constraint message_event_id_unique_idx unique,
                 room_id text,
--- middleman/migrations/003.py
+++ middleman/migrations/003.py
@@ -5,6 +5,9 @@ def migrate(store):
     store._execute("""
         drop table messages
     """)
+    store._execute("""
+        drop table massages
+    """)
     if store.db_type == "postgres":
         store._execute("""
             CREATE TABLE messages (
@@ -14,9 +17,25 @@ def migrate(store):
                 room_id text
             )
         """)
+        store._execute("""
+            CREATE TABLE massages (
+                id SERIAL PRIMARY KEY,
+                event_id text constraint message_event_id_unique_idx unique,
+                management_event_id text constraint message_management_event_id_unique_idx unique,
+                room_id text
+            )
+        """)
     else:
         store._execute("""
             CREATE TABLE messages (
+                id INTEGER PRIMARY KEY autoincrement,
+                event_id text constraint message_event_id_unique_idx unique,
+                management_event_id text constraint message_management_event_id_unique_idx unique,
+                room_id text
+            )
+        """)
+        store._execute("""
+            CREATE TABLE massages (
                 id INTEGER PRIMARY KEY autoincrement,
                 event_id text constraint message_event_id_unique_idx unique,
                 management_event_id text constraint message_management_event_id_unique_idx unique,
