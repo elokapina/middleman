f1a73822cdaea17e8db835eeeb13a41a76eb057c
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-04-22 22:44:06 +0300

    Update callbacks.py

--- middleman/callbacks.py
+++ middleman/callbacks.py
@@ -83,7 +83,7 @@ class Callbacks(object):
         if len(self.welcome_message_sent_to_room) > DUPLICATES_CACHE_SIZE:
             self.welcome_message_sent_to_room = self.welcome_message_sent_to_room[:DUPLICATES_CACHE_SIZE]
 
-    async def member(self, room: Matrix, event: RoomMemberEvent):
+    async def member(self, room: MatrixRoom, event: RoomMemberEvent) -> None:
         """Callback for when a room member event is received.
 
         Args:
@@ -112,7 +112,7 @@ class Callbacks(object):
             return
 
         # Send welcome message if configured
-        if self.config.welcome_message and room.is_group():
+        if self.config.welcome_message and room.is_group:
             if room.room_id in self.welcome_message_sent_to_room:
                 logger.debug(f"Not sending welcome message to room {room.room_id} - it's been sent already!")
                 return
