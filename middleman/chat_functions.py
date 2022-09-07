740acfa8c06669da6007c62edc212875ff601e3d
jamazi <jamazi@tutanota.com>
2022-09-08 01:21:42 +0300

    Add support to relay media messages
    This includes images, audios, videos and files.
    Relaying replies in management room will be active if
    relay_management_media option set to true
    Fixes #15

--- middleman/chat_functions.py
+++ middleman/chat_functions.py
@@ -119,3 +119,63 @@ async def send_reaction(
     except (LocalProtocolError, SendRetryError) as ex:
         logger.exception(f"Unable to send reaction to {event_id}")
         return f"Failed to send reaction: {ex}"
+
+
+async def send_media_to_room(
+    client: AsyncClient, room: str, media_type: str, body: str, media_url: str = None,
+    media_file: dict = None, media_info: dict = None, reply_to_event_id: str = None,
+) -> Union[RoomSendResponse, RoomSendError, str]:
+    """Send media to a matrix room
+
+    Args:
+        client (nio.AsyncClient): The client to communicate to matrix with
+
+        room (str): The ID or alias of the room to send the message to
+
+        body (str): The media body
+
+        media_info (dict): The media url and metadata
+
+        reply_to_event_id (str): Optional event ID that this message is a reply to.
+    """
+    try:
+        room_id = await get_room_id(client, room, logger)
+    except ValueError as ex:
+        return str(ex)
+    
+    if not (media_url or media_file):
+        logger.warning(f"Empty media url for room identifier: {room}")
+        return "Empty media url"
+
+    content = {
+        "msgtype": media_type,
+        "body": body,
+    }
+
+    if media_url:
+        content.update({"url": media_url})
+
+    if media_file:
+        content.update({"file": media_file})
+
+    if media_info:
+        content.update({"info": media_info})
+
+    # We don't store the original message content so cannot provide the fallback, unfortunately
+    if reply_to_event_id:
+        content["m.relates_to"] = {
+            "m.in_reply_to": {
+                "event_id": reply_to_event_id,
+            },
+        }
+
+    try:
+        return await client.room_send(
+            room_id,
+            "m.room.message",
+            content,
+            ignore_unverified_devices=True,
+        )
+    except (LocalProtocolError, SendRetryError) as ex:
+        logger.exception(f"Unable to send media response to {room_id}")
+        return f"Failed to send media: {ex}"
