0650538f9adb4333b2199365be55790f1909731d
botsarefuture <vuoreol@pm.me>
2023-03-10 09:30:41 +0200

    Update middleman/migrations/006.py
    Co-authored-by: Jason Robinson <mail@jasonrobinson.me>

--- middleman/migrations/006.py
+++ middleman/migrations/006.py
@@ -11,7 +11,8 @@ def migrate(store):
         store._execute("""
             CREATE TABLE directs (
                 id INTEGER PRIMARY KEY autoincrement,
-                event_id text unique,
+                room_id text unique,
+
                 room_id text
             )
         """)
